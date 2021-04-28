20968c247345ba6d3a2b1116e5ee10916c2cf14e
hyunwo4 <h161204@naver.com>
2021-04-28 21:27:12 +0900

    data\aitport.png

--- make.py
+++ make.py
@@ -2,16 +2,34 @@ from urllib.request import Request, urlopen
 import json
 import os
 
-api_url = 'https://api.github.com/repos/prajnasb/observations/'
+
+def image_download(url, filepath):
+    request = Request(url)
+    response = urlopen(request)
+
+    image_data = response.read()
+
+    file = open(filepath, 'wb')
+    file.write(image_data)
+    file.close()
+    print(url + '로 부터 ' + filepath + '에 다운로드 완료')
+    # url로 부터 filepath에 다운로드 완료
+
+mask_url = 'https://github.com/prajnasb/observations/raw/master/mask_classifier/Data_Generator/images/blue-mask.png'
+image_download(mask_url, 'data/mask.png')
+exit()
+
+
+api_url = 'https://api.github.com/repos/prajnasb/observations/contents/experiements/data/without_mask?ref=master'
 
 hds = {'User-Agent': 'Mozilla/5.0'}
 
-request = Request(api_url, header=hds)
+request = Request(api_url, headers=hds)
 response = urlopen(request)
 directory_bytes = response.read()
 directory_str = directory_bytes.decode('utf-8')
 
-content = json.loads(directory_str)
+contents = json.loads(directory_str)
 
 for i in range(len(contents)):
     content = contents[i]
@@ -27,5 +45,5 @@ for i in range(len(contents)):
 
     image_file = open('data/without_mask/' + content['name'], 'wb')
     image_file.write(image_data)
-    print('다운로드 완료(' + str(i+1) + '/' + str(len(content)) + '): ' + content['name'])
-    #다운로드 완료(10/500): 10.jpg
+    print('다운로드 완료(' + str(i + 1) + '/' + str(len(contents)) + '): ' + content['name'])
+    break
